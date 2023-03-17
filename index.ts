d58aa098ee0a8664b3e9f51870f41807f23a1365
Niklas <derNiklaas@users.noreply.github.com>
2023-03-17 14:05:36 +0100

    Added changes from the 1.2.0 api update
    Signed-off-by: Niklas <derNiklaas@users.noreply.github.com>

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import fetch from 'node-fetch';
 import throttle from 'fetch-throttle';
-const fetchMCC = throttle(fetch, 40, 60*1000);
+
+const fetchMCC = throttle(fetch, 40, 60 * 1000);
 
 export default class MCCAPI {
     private static BASE_URL = "https://api.mcchampionship.com/v1";
@@ -10,67 +11,112 @@ export default class MCCAPI {
         return await response.json()
     }
 
+    /**
+     * Returns event information about the current event cycle.
+     */
     public static async getEventInformation(): Promise<EventInformationResponse> {
         return await this.request("event") as EventInformationResponse;
     }
 
+    /**
+     * Returns all statistics in the Hall of Fame.
+     * Updates at the end of each event.
+     */
     public static async getHallOfFameStats(): Promise<HallOfFameResponse> {
         return await this.request("halloffame") as HallOfFameResponse;
     }
 
+    /**
+     * Returns all statistics in the Hall of Fame for a given game.
+     * Updates at the end of each event.
+     *
+     * @param game The key of the game you want statistics for
+     */
     public static async getHallOfFameStatsForGame(game: Game): Promise<HallOfFameGameResponse> {
         return await this.request(`halloffame/${game}`) as HallOfFameGameResponse;
     }
 
+    /**
+     * Returns summary data for the last including event scores, participants and the order of games.
+     *
+     * NOTE: Any event from Season 3 (29 and beyond) gives a full rundown of each game,
+     * including its game and individual scores. Events before Season 3 (28 and below) will receive this data "soon".
+     */
     public static async getRundown(): Promise<RundownResponse> {
         return await this.request("rundown") as RundownResponse
     }
 
+    /**
+     * Returns summary data for the event you give including event scores, participants and the order of games.
+     *
+     * NOTE: Any event from Season 3 (29 and beyond) gives a full rundown of each game,
+     * including its game and individual scores. Events before Season 3 (28 and below) will receive this data "soon".
+     *
+     * @param event the number of a given (half-)canon event
+     */
+    public static async getRundownForEvent(event: string): Promise<RundownResponse> {
+        return await this.request(`rundown/${event}`) as RundownResponse
+    }
+
+    /**
+     * Returns a list of all participants in the current event cycle.
+     */
     public static async getParticipants(): Promise<ParticipantResponse> {
         return await this.request("participants") as ParticipantResponse
     }
 
+    /**
+     * Returns a list of all participants in the given team in the current event cycle.
+     *
+     * @param team The team you want all participants of
+     */
     public static async getParticipantsOnTeam(team: Team): Promise<ParticipantTeamResponse> {
         return await this.request(`participants/${team}`) as ParticipantTeamResponse
     }
 }
 
-export interface EventInformationResponse {
+export interface BaseResponse {
+    /** The HTTP response code (e.g. 200, 429) */
     "code": number,
-    "reason": string,
+    /** The reason for the response, if applicable. */
+    "reason": string
+}
+
+export interface EventInformationResponse extends BaseResponse {
     "data": {
+        /** The date of the event in form of a JavaScript Date object. */
         "date": string,
+        /** The name of the next MCC. */
         "event": string,
+        /** URL to the YouTube embed of the update video published on the Noxcrew Channel. */
         "updateVideo": string
     }
 }
 
-export interface HallOfFameResponse {
-    "code": number,
-    "reason": string,
+export interface HallOfFameResponse extends BaseResponse {
     "data": {
         [key in Game]: HallOfFameGameData
     }
 }
 
-export interface HallOfFameGameResponse {
-    "code": number,
-    "reason": string,
+export interface HallOfFameGameResponse extends BaseResponse {
     "data": HallOfFameGameData
 }
 
 export interface HallOfFameGameData {
     [k: string]: {
+        /** The player who currently holds this record. */
         "player": string,
+        /** The value of this record (e.g. most eliminations). */
         "value": string,
+        // Internal mcc.live value (?)
         "placement": number,
+        /** Whether the record has changed hands in the last MCC. */
         "changedHands": boolean
     }
 }
 
-export interface RundownResponse {
-    "code": number,
-    "reason": string
+export interface RundownResponse extends BaseResponse {
     "data": {
         "dodgeboltData": {
             [key in Team]: string
@@ -83,30 +129,34 @@ export interface RundownResponse {
         },
         "individualScores": {
             [k: string]: number
+        },
+        "history": GameHistory,
+        "creators": {
+            [key in Team]: Array<String>
         }
     }
 }
 
-export interface ParticipantResponse {
-    "code": number,
-    "reason": string,
+export interface ParticipantResponse extends BaseResponse {
     "data": {
         [key in Team]: Array<PlayerData>
     }
 }
 
 export interface PlayerData {
+    /** The players Minecraft username. */
     "username": string,
+    /** The players Minecraft UUID. */
     "uuid": string,
+    /** The link to the players stream. */
     "stream": string
 }
 
-export interface ParticipantTeamResponse {
-    "code": number,
-    "reason": string,
+export interface ParticipantTeamResponse extends BaseResponse {
     "data": Array<PlayerData>
 }
 
+/** Internal keys for every MCC game */
 export type Game =
     "MG_ROCKET_SPLEEF"
     | "MG_SURVIVAL_GAMES"
@@ -126,5 +176,39 @@ export type Game =
     | "MG_MELTDOWN"
     | "GLOBAL_STATISTICS"
     | "LEGACY_STATISTICS"
+    /** This key does not hold any statistics. */
+    | "MG_LOCKOUT_BINGO"
+    /** This key does not hold any statistics. */
+    | "MG_FOOT_RACE"
+    /** This key does not hold any statistics.  */
+    | "MG_ROCKET_SPLEEF_OLD"
 
 export type Team = "RED" | "ORANGE" | "YELLOW" | "LIME" | "GREEN" | "CYAN" | "AQUA" | "BLUE" | "PURPLE" | "PINK"
+
+export type GameHistory = {
+    /** The first game played. */
+    "0": GameInformation,
+    /** The second game played. */
+    "1": GameInformation,
+    /** The third game played. */
+    "2": GameInformation,
+    /** The forth game played. */
+    "3": GameInformation,
+    /** The fifth game played. */
+    "4": GameInformation,
+    /** The sixth game played. */
+    "5": GameInformation,
+    /** The seventh game played. */
+    "6": GameInformation,
+    /** The eighth game played. */
+    "7": GameInformation
+}
+
+export type GameInformation = {
+    /** The score multiplier of the game. Values can be 1, 1.5, 2, 2.5 and 3. */
+    "multiplier": number,
+    /** The key of the game played. */
+    "game": Game,
+    /** A zero based index of when the game was played in the event. */
+    "index": number
+}
--- test.ts
+++ test.ts
@@ -1,22 +1,27 @@
 import MCCAPI from "./index.js"
 
 async function run() {
+
     let HOF = await MCCAPI.getHallOfFameStats();
-    console.log(HOF.data.MG_ROCKET_SPLEEF["Most eliminations"]);
+    console.log("Most eliminations in RS: " + JSON.stringify(HOF.data.MG_ROCKET_SPLEEF["Most eliminations"]));
+
     let HOF_RS = await MCCAPI.getHallOfFameStatsForGame("MG_ROCKET_SPLEEF");
-    console.log(HOF_RS.data["Most eliminations"]);
+    console.log("Most eliminations in RS: " + JSON.stringify(HOF_RS.data["Most eliminations"]));
 
     let red = await MCCAPI.getParticipantsOnTeam("RED");
-    console.log(red.data[0].stream);
+    console.log("First player on red: " + red.data[0].username);
 
     let all = await MCCAPI.getParticipants();
-    console.log(all.data.RED[0]);
+    console.log("The user object of the first player on red: " + JSON.stringify(all.data.RED[0]));
 
     let rundown = await MCCAPI.getRundown();
-    console.log(rundown.data.eventScores.RED);
+    console.log("The amount of coins of red in the last event: " + rundown.data.eventScores.RED);
+
+    let rundownMCC3 = await MCCAPI.getRundownForEvent("3");
+    console.log("The 4th game in MCC3: " + JSON.stringify(rundownMCC3.data.history["3"]));
 
     let eventInfo = await MCCAPI.getEventInformation();
-    console.log(eventInfo.data.updateVideo);
+    console.log("The name of the next MCC event: " + eventInfo.data.event);
 }
 
 run().then();
